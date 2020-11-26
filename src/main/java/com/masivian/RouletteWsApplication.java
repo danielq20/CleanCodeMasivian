e8a9f789ef71002680b9726f1bb2f98f1cba56c9
danielq20 <danielquin2020@gmail.com>
2020-11-25 20:15:19 -0500

    Basic configuration of project (structure, packages)

---
+++ src/main/java/com/masivian/RouletteWsApplication.java
@@ -0,0 +1,22 @@
+package com.masivian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.lang.Object;
+
+@SpringBootApplication
+@EnableRedisRepositories
+public class RouletteWsApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteWsApplication.class, args);
+	}
+
+}
